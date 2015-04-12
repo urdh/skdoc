892062fd50aafdbd87da931f2206e2e7fd18670a
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2015-04-12 16:05:48 +0200

    Add CTAN deployment script

---
+++ deploy-ctan.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_TAG" ]]; then
+  printf "\033[33;1mCommit is tagged with $TRAVIS_TAG, deploying to CTAN...\033[0m\n"
+  # 1. Install ctanupload
+  tlmgr install ctanupload || exit 1;
+  # 2. Set up and sanity-check environment
+  [[ "$CONTRIBUTION" && "$NAME" && "$EMAIL" && "$DIRECTORY" ]] || exit 2;
+  [[ "$LICENSE" ]] || LICENSE="other-nonfree";
+  [[ "$LICENSE" == "free" && "$FREEVERSION" ]] || FREEVERSION="other-free";
+  # 3. Set the file to upload
+  [[ "$1" ]] && FILE="$1"
+  [[ "$FILE" ]] || FILE=$(find . -name '*.tar.gz' | head);
+  echo "Deploying file $FILE"
+  export CONTRIBUTION NAME EMAIL SUMMARY DIRECTORY NOTES LICENSE FREEVERSION FILE
+  # 4. Upload package
+  ctanupload -y --version=$TRAVIS_TAG --notes="Package contains TDS zip file" --DoNotAnnounce=1
+fi
